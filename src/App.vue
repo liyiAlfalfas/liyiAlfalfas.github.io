94ecd2c92146f7893e15cf134fd8b9396d38f1de
liyiAlfalfas <151724317+liyiAlfalfas@users.noreply.github.com>
2024-03-12 15:35:24 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div id="demo"></div>
+  </div>
+</template>
+<script>
+import Pdfh5 from 'pdfh5';
+export default {
+  name: 'App',
+  data() {
+    return {
+      pdfh5: null
+    };
+  },
+  mounted() {
+    this.pdfh5 = new Pdfh5('#demo', {
+      pdfurl: "./static/git.pdf"
+    });
+    this.pdfh5.on("scroll", function (scrollTop, currentNum) {
+      console.log("scrollTop:" + scrollTop, "currentNum:" + currentNum);
+    });
+
+  }
+};
+</script>
+
+<style>
+@import 'pdfh5/css/pdfh5.css';
+
+* {
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}</style>
